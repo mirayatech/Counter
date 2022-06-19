a074dd9dc70ca67142d3fa4465c2d4148c3efaf9
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-06-19 06:25:13 +0200

    delete uneccessary comment

--- app.js
+++ app.js
@@ -1,23 +1,15 @@
-// Selectors
 let count = 0;
-
-// Decrease Button
 document.getElementById("decrease__btn").onclick = function () {
   count -= 1;
-
   document.getElementById("countlabel").innerHTML = count;
 };
 
-// Increase Button
 document.getElementById("increase__btn").onclick = function () {
   count += 1;
-
   document.getElementById("countlabel").innerHTML = count;
 };
 
-// Reset Button
 document.getElementById("reset__btn").onclick = function () {
   count = 0;
-
   document.getElementById("countlabel").innerHTML = count;
 };
